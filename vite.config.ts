d5b955ace5289e52cae29286a803da2159e5dd76
uticaa <58889268+uticaa@users.noreply.github.com>
2026-07-26 16:15:26 +0530

    Update base URL in vite.config.ts
    Changed the base URL for the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default {
-  base: '/',
+  base: '/yutika/',  // Change from '/' to '/yutika/'
   plugins: [react()],
   build: {
     outDir: 'dist',
