87bdc9adaa9d40799b8ef8c5013479c5adc47d3b
uticaa <58889268+uticaa@users.noreply.github.com>
2026-07-26 16:04:48 +0530

    Fix export syntax in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default {
   base: '/',
   plugins: [react()],
   build: {
@@ -10,4 +10,4 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: true
   }
-})
+}
